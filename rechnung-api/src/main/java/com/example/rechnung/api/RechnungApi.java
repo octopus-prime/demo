b9d85a5f07f18ccb90e567eec2ecf48f480b15d5
octopus-prime <mike.gresens@gmail.com>
2019-02-10 16:46:09 +0100

    cleanup

--- rechnung-api/src/main/java/com/example/rechnung/api/RechnungApi.java
+++ rechnung-api/src/main/java/com/example/rechnung/api/RechnungApi.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-//import org.springframework.validation.annotation.Validated;
-
 public interface RechnungApi {
 
     @PostMapping(path = "rechnungen", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    RechnungDto createRechnung(/*@Validated*/ @RequestBody BestellungDto bestellung);
+    RechnungDto createRechnung(@RequestBody BestellungDto bestellung);
 
     @GetMapping(path = "rechnungen/{rechnungId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
