f666ca1d74e92839948e7afe4a8f86c2a19735b6
octopus-prime <mike.gresens@gmail.com>
2019-07-06 13:51:25 +0200

    clean up

--- rechnung-service/src/test/java/com/example/rechnung/service/RestAssuredExtension.java
+++ rechnung-service/src/test/java/com/example/rechnung/service/RestAssuredExtension.java
@@ -3,7 +3,6 @@ package com.example.rechnung.service;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
@@ -12,8 +11,6 @@ import org.junit.jupiter.api.extension.ParameterResolver;
 public class RestAssuredExtension implements ParameterResolver {
 
     private final RequestSpecification specification = new RequestSpecBuilder()
-            .setContentType(ContentType.JSON)
-            .setAccept(ContentType.JSON)
             .addFilter(new RequestLoggingFilter())
             .addFilter(new ResponseLoggingFilter())
             .build();
