fc99a954b1d12186a9f026505f57ea910f2790b4
octopus-prime <mike.gresens@gmail.com>
2019-11-23 10:52:14 +0100

    feign config

--- rechnung-service/src/main/java/com/example/rechnung/service/KundeClient.java
+++ rechnung-service/src/main/java/com/example/rechnung/service/KundeClient.java
@@ -3,6 +3,6 @@ package com.example.rechnung.service;
 import com.example.kunde.api.KundeApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "kunde-service", path = "/kunde-api", decode404 = true)
+@FeignClient(name = "kunde-service", path = "/kunde-api")
 interface KundeClient extends KundeApi {
 }
--- rechnung-service/src/main/java/com/example/rechnung/service/PreisClient.java
+++ rechnung-service/src/main/java/com/example/rechnung/service/PreisClient.java
@@ -3,6 +3,6 @@ package com.example.rechnung.service;
 import com.example.preis.api.PreisApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "preis-service", path = "/preis-api", decode404 = true)
+@FeignClient(name = "preis-service", path = "/preis-api")
 interface PreisClient extends PreisApi {
 }
