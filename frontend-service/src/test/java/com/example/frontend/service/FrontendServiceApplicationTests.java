bab7063124fd21c760f735452bfe431cfc5b8145
octopus-prime <mike.gresens@gmail.com>
2020-06-01 15:30:33 +0200

    frontend with gateway

---
+++ frontend-service/src/test/java/com/example/frontend/service/FrontendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontend.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
